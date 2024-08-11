901fecd63a79cc6d76c87562d895ceb630718bd5
lusegiven <luciasgiven@gmail.com>
2024-08-11 23:58:59 +0300

    Add new note button in home working

--- frontend/todolist/src/pages/Home/Home.jsx
+++ frontend/todolist/src/pages/Home/Home.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
+import AddEditNotes from './AddEditNotes'
+import Modal from "react-modal"
 
 const home = () => {
+
+  const [openAddEditModal,setOpenAddEditModal] = useState({
+    isShown:false,
+    type:"add",
+    data:null,
+  });
+
   return (
     <>
     <Navbar/>
+    <div className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-4 mt-8 ml-5'>
+        <NoteCard 
+          title = "Meeting on 7th April"
+          date="3rd April 2024"
+          content="meeting very important one on this date not to miss!"
+          tags="#meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
+    </div>
+    <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-link absolute right-10 bottom-10' 
+        onClick={()=>{
+          setOpenAddEditModal({isShown:true, type:"add",data:null})
+        }}>
+      <MdAdd className='text-[32px] text-white'/>
+    </button>
+
+    <Modal 
+      isOpen={openAddEditModal.isShown}
+      onRequestClose={() => {}}
+      style={{
+        overlay:{
+          backgroundColor: "rgba(0,0,0,0,2)",
+        },
+      }}
+      contentLabel=""
+      className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-12 p-5 "
+      >
+
+      <AddEditNotes 
+        onClose={() => {
+          setOpenAddEditModal({isShown:false, type:"add", data:null})
+        }}
+      />
+    </Modal>
     </>
   )
 }
